ea76bf06b9a34f4897a8ca21ac7ed53d83203b6d
Carlos <gohercarlos@gmail.com>
2024-05-26 19:18:39 -0500

    Init Proyect API GATEWAY

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_api_gateway/MicECommerceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicECommerceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicECommerceApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/whiskers/mic_e_commerce_api_gateway/MicECommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicECommerceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
